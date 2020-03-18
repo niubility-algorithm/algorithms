15d364859d79aadcccde714cd32b1d2db7a3fd35
fuzhengwei <184172133@qq.com>
2020-03-18 14:25:22 +0800

    小傅哥 | 1. 两数之和

--- LeetCode/Two Sum/code.js
+++ LeetCode/Two Sum/code.js
@@ -7,27 +7,37 @@ const logger = new LogTracer('两数之和');
 Layout.setRoot(new VerticalLayout([tracerArray, tracerBit, logger]));
 
 var twoSum = function (nums, target) {
-    var volume = 32;
+    var volume = 16;
     var bitMode = volume - 1;
-    var t = new Array(32).fill(0);
-
+    var t = new Array(16).fill(0);
+    // 设置bit数组
     tracerBit.set(t);
 
     for (var i = 0; i < nums.length; i++) {
+        tracerArray.select(i);
+        Tracer.delay();
+
         var c = (target - nums[i]) & bitMode;
         if (t[c] !== 0) return [t[c] - 1, i];
-        t[nums[i] & bitMode] = i + 1;
+
+        var bitIdx = nums[i] & bitMode;
+        var bitVal = i + 1;
+        t[bitIdx] = bitVal;
+        logger.println('数据存放-> bitIdx：' + bitIdx + ' bitVal：' + bitVal);
+
+        tracerArray.deselect(i);
+        tracerBit.patch(bitIdx, bitVal);
     }
     return [-1, -1];
 };
 
 (function main() {
-    var nums = [2, 7, 11, 15];
+    var nums = [3, 7, 1, 10, 4, 2];
     tracerArray.set(nums);
 
     var idx = twoSum(nums, 9);
 
-    logger.println(idx);
+    logger.println('两数之和(Idx)：' + '[' + idx + ']');
 
 })();
 
